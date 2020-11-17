40d5da3f32a74d2581622132317acf8ecd831769
rmartinezbsas <73311475+rmartinezbsas@users.noreply.github.com>
2020-11-16 21:06:45 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def cargar(archivo, campos):
 
         with open(archivo, formato, newline='') as file:
             file_guarda = csv.writer(file)
-            #file_guarda.writerow(campos)
             file_guarda.writerows(lista_empleados)
             print("se guardo correctamente")
             print()
@@ -52,7 +51,96 @@ def cargar(archivo, campos):
     except IOError:
         print("Ocurrio un error con el archivo")
 
+def informar(archivo,archivo2):
+      guardar2=False
+      while guardar2==False:
+            archivo_ei=(input("Con Cual Archivo de empleados desea trabajar?: "))
+            if os.path.exists(archivo_ei):
+              archivo=archivo_ei
+              guardar2=True
+            else:
+              print("El archivo no existe. Quiere elegir otro?: ")
+              eleccion_z=(input("si/no?: "))
+              if eleccion_z=="no":
+                  print("Muchas gracias. Lo guiaremos al menu principal por si quiere crear un archivo de empleados")
+                  print()
+                  menu()
+       
 
+      archivo_vi=(input("Con Cual Archivo de viaticos desea trabajar ?"))
+      if os.path.exists(archivo_vi):
+        archivo2=archivo_vi
+      else:
+        print("El archivo no existe. Quiere trabajar con el archivo",archivo2,":?")
+        eleccion_aqn=(input("si/no:? "))
+        if eleccion_aqn=="si":
+          archivo2=archivo2
+        else:
+          print("No contamos con otro archivo. Lo siento lo redigiremos al menu principal")
+          menu()
+
+
+    
+      
+
+      empleado_a=open(archivo) 
+      viaticos_a=open(archivo2)
+      empleado_csv = csv.reader(empleado_a)
+      viaticos_csv = csv.reader(viaticos_a)
+
+      
+      empleado=next(empleado_csv,None)
+      viaticos=next(viaticos_csv,None)
+      legajo_usuario=input("Ingrese numero de legajo buscado: ")
+      print()
+      bandera2=False    
+      while bandera2==False:           
+             try:
+                   int(legajo_usuario)
+                   bandera2=True
+             except ValueError:
+                   print()
+                   print("Necesita ingresar un numero entero en el Legajo")
+                   legajo_usuario=input("Ingrese numero de legajo correcto: ")
+                   print()
+      count=0            
+      saldo_viaticos_a=0
+      saldo_viaticos_t=0
+      while (empleado):
+              if (empleado[0]==legajo_usuario):
+                  while viaticos:   
+                                     
+                    if(viaticos[0]==legajo_usuario):
+                                                                   
+                       while (viaticos[0]==legajo_usuario):
+                         count=count+1
+                         saldo_viaticos_a= int(viaticos[1])
+                         saldo_viaticos_t= saldo_viaticos_t+saldo_viaticos_a
+                         viaticos= next(viaticos_csv, None)
+                       
+                       if saldo_viaticos_t > 5000:
+                            limite= 5000
+                            diferencia= saldo_viaticos_t-limite
+                            print(f"Legajo {empleado[0]} {empleado[2]} {empleado[1]} gasto",saldo_viaticos_t, "y se ha pasado del presupuesto por $",diferencia)
+                            menu()
+                       else:
+                         print(f"Legajo {empleado[0]} {empleado[2]} {empleado[1]} gasto",saldo_viaticos_t, ":")
+                       print()
+                       menu()
+                       
+                    viaticos= next(viaticos_csv, None)
+              else:
+                empleado=next(empleado_csv,None)   
+      print("El legajo no existe")
+      print("Lo guiaremos al menu principal. Puede intentar nuvemente si desea")
+      print()
+                
+              
+      
+
+     
+      empleado_a.close()
+      viaticos_a.close()
 
 def menu():
 
@@ -68,9 +156,9 @@ def menu():
            cargar(ARCHIVO, CAMPOS)
         if opcion == "2":
            informar(ARCHIVO,ARCHIVO2)
-           pass
         if opcion == "3":
             exit()
+      
         else:
             print("Por favor elija una opcion valida")
 
